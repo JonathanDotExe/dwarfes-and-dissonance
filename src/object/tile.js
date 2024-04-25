46bf5803348c3efd5d39d9a2077790ea7285ad2c
DatDonathan <jojo08082003@gmail.com>
2024-04-25 08:28:02 +0200

    Add images

--- src/object/tile.js
+++ src/object/tile.js
@@ -12,4 +12,12 @@ export class Tile {
     }
 }
 
-//TODO tile constants
+function loadTileImage(name) {
+    let image = new Image();
+    image.src = "res/tiles/" + name + ".png"
+    return image;
+}
+
+export const GRASS_TILE = new Tile(loadTileImage("grass"), false, false);
+export const SAND_TILE = new Tile(loadTileImage("sand"), false, false);
+export const WATER_TILE = new Tile(loadTileImage("water"), false, true);
--- src/world.js
+++ src/world.js
@@ -4,11 +4,31 @@ export const WORLD_SIZE = 256;
 export class GameWorld {
 
     constructor() {
-        this.tiles = [];//TODO initialize tile array
-        this.objects = [];
+        this._tiles = [];//TODO initialize tile array
+        this._objects = [];
 
     }
 
-    //TODO
+    update(deltaTime) {
+
+    }
+
+    draw(ctx) {
+
+    }
+
+    addObject(obj) {
+        //TODO exception handling
+        _obj.init(this);
+        this._objects.addObject(obj);
+    }
+
+    setTile(x, y, tile) {
+        this._tiles[y][x] = tile;
+    }
+
+    getTile(x, y) {
+        return this._tiles[y][x];
+    }
 
 }
