172d20d392c1259f3f7c37cdb1bfe87c86f9f92b
DatDonathan <jojo08082003@gmail.com>
2024-05-08 11:00:33 +0200

    Generate trees

--- src/worldgenerator.js
+++ src/worldgenerator.js
@@ -1,15 +1,19 @@
 import { ValueNoise } from '../node_modules/value-noise-js/dist/value-noise.mjs'
+import { Tree } from './object/static/tree.js';
 import { GRASS_TILE, SAND_TILE, STONE_TILE, WATER_TILE } from './object/tile.js';
 
+const TREE_CHUNK_SIZE = 16;
 
 export class WorldGenerator {
 
     constructor(seed) {
         this._noise = new ValueNoise(seed);
+        this._seed = seed;
         this.scale = 0.05;
     }
 
     generate(world, x, y, width, height) {
+        const rng = new Math.seedrandom(this._seed + x * 31 + y * 73);
         //Generate basic terrain
         for (let i = 0; i < width; i++) {
             for (let j = 0; j < height; j++) {
@@ -27,7 +31,22 @@ export class WorldGenerator {
                 world.setTile(x + i, y + j, tile);
             }
         }
+        //Trees
+        //Single trees
+        for (let i = 0; i < width; i += TREE_CHUNK_SIZE) {
+            for (let j = 0; j < height; j +=TREE_CHUNK_SIZE) {
+                const numTrees = Math.floor(rng() * 10);
+                for (let k = 0; k < numTrees; k++) {
+                    const treeX = x + i + Math.floor(rng() * TREE_CHUNK_SIZE);
+                    const treeY = y + j + Math.floor(rng() * TREE_CHUNK_SIZE);
 
+                    //Only place trees on grass
+                    if (world.getTile(treeX, treeY) == GRASS_TILE) {
+                        world.addObject(new Tree(treeX, treeY));
+                    }
+                }
+            }
+        }
 
     }
 
